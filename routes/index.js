6eeaf8c7ceea0ae084be26ccc924bc5e728daf35
Guang Yang <gy2237@columbia.edu>
2017-06-10 13:18:00 -0400

    add sign up page

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,11 @@
 var express = require('express');
 var router = express.Router();
 var Product = require('../models/product');
+var csrf = require('csurf');
+
+// csurf middleware
+var csrfProtection = csrf();
+router.use(csrfProtection);
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -12,6 +17,10 @@ router.get('/', function(req, res, next) {
     }
     res.render('index', { title: 'Express', productChunks: productChunks });
   });
+});
+
+router.get('/user/signup', function(req, res, next) {
+  res.render('user/signup', {title: 'login', csrfToken: req.csrfToken()});
 });
 
 module.exports = router;
